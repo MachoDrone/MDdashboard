6ead8f7e46bd8e1103150ae56230223176cabcbe
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-19 19:44:08 -0400

    Update MDd.sh

--- MDd.sh
+++ MDd.sh
@@ -111,7 +111,7 @@ edit_logind_conf() {
 
 # Edit sudoer for passwordless sudo
 # sudoers entry
-sudoers_entry="$SUDO_USER ALL=(ALL) NOPASSWD:ALL
+sudoers_entry="$SUDO_USER ALL=(ALL) NOPASSWD:ALL"
 
 # add the entry to sudoers using a temp file
 temp_file=$(mktemp)
